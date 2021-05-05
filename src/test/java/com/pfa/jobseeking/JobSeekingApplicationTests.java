f5b392cf114a90a8c615c412d48e63f0a388ea5c
Youness IHARCHINE <youness.iharchine@gmail.com>
2021-05-05 16:10:16 +0000

    initial commit

---
+++ src/test/java/com/pfa/jobseeking/JobSeekingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfa.jobseeking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSeekingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
