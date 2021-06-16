b8b3f6bb02664c5586797426c17a85481961a335
Youness IHARCHINE <youness.iharchine@gmail.com>
2021-06-16 13:28:25 +0100

    Test Functionality : Save and Get an image
    + added test functionality of saving an image by sending a Post request containing its Base64 encoded value to "/api/images"
    + added test functionality of getting the Base64 encoded value of an image by sending a Get request to "/api/images"

--- src/main/java/com/pfa/jobseeking/rest/controller/TestController.java
+++ src/main/java/com/pfa/jobseeking/rest/controller/TestController.java
@@ -1,10 +1,17 @@
 package com.pfa.jobseeking.rest.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.security.Principal;
+import java.util.Base64;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pfa.jobseeking.model.City;
@@ -48,6 +55,24 @@ public class TestController {
 	
 	@Autowired
 	PasswordEncoder passwordEncoder;
+	
+	@Value("${storage.images.basePath}")
+	String path;
+	
+	
+	@PostMapping("/api/images")
+	void createImage(@RequestBody String image) throws IOException {
+		byte[] imageBytes = Base64.getDecoder().decode(image);
+		FileUtils.writeByteArrayToFile(new File(path+"\\image-test.jpeg"), imageBytes);
+	}
+	
+	
+	
+	@GetMapping("/api/images")
+	String showImage() throws IOException {
+		byte[] imageBytes = FileUtils.readFileToByteArray(new File(path+"\\image-test.jpeg"));
+		return Base64.getEncoder().encodeToString(imageBytes);
+	}
 	
 	
 	@GetMapping("/test")
