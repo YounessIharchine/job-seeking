a1ca3a9570394e07cbb069e7e6264de185bda925
Youness IHARCHINE <youness.iharchine@gmail.com>
2021-06-22 15:31:26 +0100

    Test : show a file / download a file
    + added test endpoint for downloading a file
    + added test endpoint for showing a file

--- src/main/java/com/pfa/jobseeking/rest/controller/TestController.java
+++ src/main/java/com/pfa/jobseeking/rest/controller/TestController.java
@@ -2,16 +2,25 @@ package com.pfa.jobseeking.rest.controller;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.Principal;
 import java.util.Base64;
 
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pfa.jobseeking.model.user.User;
@@ -40,8 +49,7 @@ public class TestController {
 	void createImage(@RequestBody String image) throws IOException {
 		byte[] imageBytes = Base64.getDecoder().decode(image);
 		FileUtils.writeByteArrayToFile(new File(path+"\\image-test.jpeg"), imageBytes);
-	}
-	
+	}	
 	
 	
 	@GetMapping("/api/images")
@@ -49,6 +57,35 @@ public class TestController {
 		byte[] imageBytes = FileUtils.readFileToByteArray(new File(path+"\\image-test.jpeg"));
 		return Base64.getEncoder().encodeToString(imageBytes);
 	}
+	
+	
+	@GetMapping(
+			value = "/api/get-image-with-media-type", 
+			produces = MediaType.APPLICATION_PDF_VALUE
+			)
+	public @ResponseBody byte[] getImageWithMediaType() throws IOException {
+		byte[] imageBytes = FileUtils.readFileToByteArray(new File(path+"\\documents\\document-6.pdf"));
+	    return imageBytes;
+	}
+	
+	@GetMapping("/api/image")
+	public ResponseEntity<Resource> download(@RequestParam("image") String image) throws IOException {
+        File file = new File(path + File.separator + image + ".png");
+
+        HttpHeaders header = new HttpHeaders();
+        header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=img.png");
+        header.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        header.add("Pragma", "no-cache");
+        header.add("Expires", "0");
+
+        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(Paths.get(file.getAbsolutePath())));
+
+        return ResponseEntity.ok()
+                .headers(header)
+                .contentLength(file.length())
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(resource);
+    }
 	
 	
 	@GetMapping("/test")
