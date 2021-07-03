ff1ed36f216273ec030c6e0e302567937bca8f6e
Youness IHARCHINE <youness.iharchine@gmail.com>
2021-07-03 17:05:36 +0100

    Update : find companies
    + find companies now checks if the company is verfied

--- src/main/java/com/pfa/jobseeking/repository/CompanyRepository.java
+++ src/main/java/com/pfa/jobseeking/repository/CompanyRepository.java
@@ -11,19 +11,24 @@ public interface CompanyRepository extends CrudRepository<Company, Integer> {
 
 	Company findCompanyByName(String name);
 	
+	@Query("SELECT c FROM Company c "
+			+ "WHERE c.isVerified = TRUE")
 	List<Company> findAll();
 	
 	@Query("SELECT c FROM Company c "
-			+ "WHERE c.name LIKE %?1%")
+			+ "WHERE c.name LIKE %?1% "
+			+ "AND c.isVerified = TRUE")
 	List<Company> findAllByName(String name);
 
 	@Query("SELECT c FROM Company c "
-			+ "WHERE c.domain.name = ?1")
+			+ "WHERE c.domain.name = ?1"
+			+ "AND c.isVerified = TRUE")
 	List<Company> findAllByDomain(String domain);
 
 	@Query("SELECT c FROM Company c "
 			+ "WHERE c.name LIKE %?1% "
-			+ "AND c.domain.name = ?2")
+			+ "AND c.domain.name = ?2"
+			+ "AND c.isVerified = TRUE")
 	List<Company> findAllByNameAndDomain(String name, String domain);
 
 }
